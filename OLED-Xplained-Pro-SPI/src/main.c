f9c4ec911cfff3b41b34e47d7b15fa0b2b9a2e7c
RogerPina2 <roger.pina12@hotmail.com>
2020-04-06 16:25:54 -0300

    commit inicial

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -4,6 +4,40 @@
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
+#define BUT1_PIO            PIOD
+#define BUT1_PIO_ID         16
+#define BUT1_PIO_IDX        28
+#define BUT1_PIO_IDX_MASK   (1u << BUT1_PIO_IDX)
+
+#define BUT2_PIO			PIOC
+#define BUT2_PIO_ID			ID_PIOC
+#define BUT2_PIO_IDX		31
+#define BUT2_PIO_IDX_MASK	(1u << BUT2_PIO_IDX)
+
+#define BUT3_PIO			PIOA
+#define BUT3_PIO_ID			ID_PIOA
+#define BUT3_PIO_IDX		19
+#define BUT3_PIO_IDX_MASK	(1 << BUT3_PIO_IDX)
+
+
+/**
+* callback do botao1
+*/
+void but1_callback(void){
+}
+
+/**
+* callback do botao2
+*/
+void but2_callback(void){
+}
+
+/**
+* callback do botao3
+*/
+void but3_callback(void){
+}
+
 int main (void)
 {
 	board_init();
